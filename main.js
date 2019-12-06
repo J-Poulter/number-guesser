6a8a448a4efcc72681ee9cd3dd4b75d87488d96a
Charlie Bandstra <bandstracharlie@gmail.com>
2019-12-06 16:04:54 -0700

    Created variables for card names and guesses

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ var challOneName = document.querySelector('.name1');
 var challOneGuess = document.querySelector('.guess1');
 var challTwoName = document.querySelector('.name2');
 var challTwoGuess = document.querySelector('.guess2');
+var guessBoxes = document.querySelectorAll('.guess-boxes');
 
 var submitGuess = document.querySelector('.sub-guess');
 var clearButton = document.querySelector('.clear-button');
@@ -28,3 +29,7 @@ function checkSubmitStatus() {
   submitGuess.disabled = true;
 }
 }
+var nameOne = document.querySelector('.card-name1');
+var guessOne = document.querySelector('.card-guess1');
+var nameTwo = document.querySelector('.card-name2');
+var guessTwo = document.querySelector('.card-guess2');
