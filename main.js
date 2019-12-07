be86119814fea9cd0b10e2cedc39fbca5ed9abdc
Charlie Bandstra <bandstracharlie@gmail.com>
2019-12-06 19:48:25 -0700

    Added funtionality to have a response comment based on guess accuracy

--- main.js
+++ main.js
@@ -1,62 +1,91 @@
-var challOneName = document.querySelector('.name1');
-var challOneGuess = document.querySelector('.guess1');
-var challTwoName = document.querySelector('.name2');
-var challTwoGuess = document.querySelector('.guess2');
-var guessBoxes = document.querySelectorAll('.guess-boxes');
+var challOneName = document.querySelector(‘.name1’);
+var challOneGuess = document.querySelector(‘.guess1’);
+var challTwoName = document.querySelector(‘.name2’);
+var challTwoGuess = document.querySelector(‘.guess2’);
+var guessBoxes = document.querySelectorAll(‘.guess-boxes’);
+var guessComment1 = document.querySelector(‘.guess-comment1’);
+var guessComment2 = document.querySelector(‘.guess-comment2’);
 
-var submitGuess = document.querySelector('.sub-guess');
-var clearButton = document.querySelector('.clear-button');
+var submitGuess = document.querySelector(‘.sub-guess’);
+var clearButton = document.querySelector(‘.clear-button’);
 
-challOneName.addEventListener('change', activateClear);
-challOneGuess.addEventListener('change', activateClear);
-challTwoName.addEventListener('change', activateClear);
-challTwoGuess.addEventListener('change', activateClear);
+challOneName.addEventListener(‘change’, activateClear);
+challOneGuess.addEventListener(‘change’, activateClear);
+challTwoName.addEventListener(‘change’, activateClear);
+challTwoGuess.addEventListener(‘change’, activateClear);
 
 function activateClear() {
   clearButton.disabled = false;
 }
 
-challOneName.addEventListener('input', checkSubmitStatus);
-challOneGuess.addEventListener('input', checkSubmitStatus);
-challTwoName.addEventListener('input', checkSubmitStatus);
-challTwoGuess.addEventListener('input', checkSubmitStatus);
+challOneName.addEventListener(‘input’, checkSubmitStatus);
+challOneGuess.addEventListener(‘input’, checkSubmitStatus);
+challTwoName.addEventListener(‘input’, checkSubmitStatus);
+challTwoGuess.addEventListener(‘input’, checkSubmitStatus);
 
 function checkSubmitStatus() {
-  if(challOneName.value != '' && challTwoName.value != '' &&
-  challOneGuess.value != '' && challOneGuess.value != '') {
+  if(challOneName.value != ‘’ && challTwoName.value != ‘’ &&
+  challOneGuess.value != ‘’ && challOneGuess.value != ‘’) {
   submitGuess.disabled = false}
   else {
   submitGuess.disabled = true;
 }
 }
 
-clearButton.addEventListener('click', eraseInputs);
+clearButton.addEventListener(‘click’, eraseInputs);
 
 function eraseInputs() {
   for (var i = 0; i < guessBoxes.length; i++) {
-    guessBoxes[i].value = '';
+    guessBoxes[i].value = ‘’;
   }
 }
 
-var nameOne = document.querySelector('.card-name1');
-var guessOne = document.querySelector('.card-guess1');
-var nameTwo = document.querySelector('.card-name2');
-var guessTwo = document.querySelector('.card-guess2');
+var nameOne = document.querySelector(‘.card-name1’);
+var guessOne = document.querySelector(‘.card-guess1’);
+var nameTwo = document.querySelector(‘.card-name2’);
+var guessTwo = document.querySelector(‘.card-guess2’);
 
-submitGuess.addEventListener('click', latestGuess);
+submitGuess.addEventListener(‘click’, latestGuess);
 
-var guessPair = document.querySelectorAll('.guess-pair');
-
+var guessPair = document.querySelectorAll(‘.guess-pair’);
 function eraseGuesses() {
   for (var i = 0; i < guessPair.length; i++) {
-    guessPair[i].value = '';
+    guessPair[i].value = ‘’;
   }
 }
+
 function latestGuess() {
  nameOne.innerText = challOneName.value;
  nameTwo.innerText = challTwoName.value;
  guessOne.innerText = challOneGuess.value;
  guessTwo.innerText = challTwoGuess.value;
-
  eraseGuesses();
+ guessResponse1();
+ guessResponse2();
+}
+
+var randomNum = 50;
+
+function guessResponse1() {
+  if (guessOne.innerText == randomNum) {
+    guessComment1.innerText = “Boom!“;
+  }
+  else if(guessOne.innerText < randomNum) {
+    guessComment1.innerText = “That’s Too Low!“;
+  }
+  else if(guessOne.innerText > randomNum) {
+    guessComment1.innerText = “That’s Too High!“;
+  }
+}
+
+function guessResponse2() {
+  if (guessTwo.innerText == randomNum) {
+    guessComment2.innerText = “Boom!“;
+  }
+  else if(guessTwo.innerText < randomNum) {
+    guessComment2.innerText = “That’s Too Low!“;
+  }
+  else if(guessTwo.innerText > randomNum) {
+    guessComment2.innerText = “That’s Too High!“;
+  }
 }
